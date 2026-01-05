c004a6dde90cb963f8b366906923a79d474ad871
Kudriavets Valeria <vvkudriavetsvaleria@gmail.com>
2026-01-05 16:40:49 +0200

    Добавленна иконка приложения

--- main.cpp
+++ main.cpp
@@ -15,9 +15,11 @@ int main(int argc, char *argv[])
     file.open(QFile::ReadOnly);
     a.setStyleSheet(file.readAll());
 
+    // Установка иконки приложения
+    a.setWindowIcon(QIcon(":/image/TM_Logo.png"));
+
     // Установка иконки для окна
     MainWindow w;
-    w.setWindowIcon(QIcon(":images/TM.png"));  // Устанавливаем иконку из ресурсов
 
     // Применение стилей для окна
     w.setStyleSheet("QWidget { border-radius: 50px; }");
